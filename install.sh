f9e95a9423c466245f314937471667fab448020d
Nicolas Crovatti <nicolas.crovatti@gmail.com>
2011-06-30 09:16:21 +0200

    Adding colorsrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ prefix=`pwd`
 rm -rfv ~/.vim/
 cp -vir $prefix/.vimrc ~/
 cp -vir $prefix/.bashrc ~/
+cp -vir $prefix/.colorsrc ~/
 cp -vr $prefix/.vim ~/
 
 . ~/.bashrc
--- re-source.sh
+++ re-source.sh
@@ -3,6 +3,7 @@
 prefix=`pwd`
 
 cp -v ~/.bashrc $prefix
+cp -v ~/.colorsrc $prefix
 cp -v ~/.vimrc $prefix
 cp -vr ~/.vim $prefix
 rm $prefix/.vim/.netrwhist
