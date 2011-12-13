56d91805b51da95def44d3434c37eced592372fa
Nicolas Crovatti <nicolas.crovatti@gmail.com>
2011-12-13 12:10:19 +0100

    xmonad: added apps shifting to workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,12 +6,24 @@ import XMonad.Actions.CycleWS
 import XMonad.Layout.Spacing
 import XMonad.Hooks.ManageDocks
 
+myWorkspaces = ["1:web", "2:code", "3:chrome", "4:im", "5:chat",
+                "6:mail", "7", "8", "9"]
+myHookManager = composeAll
+	[ resource =? "Do" --> doIgnore -- Leave Gnome Do alone.
+	, className =? "Pidgin" --> doShift "4:im" -- Force to IM workspace.
+	, className =? "Skype" --> doShift "4:im" -- Force to Skype workspace.
+	, className =? "Firefox" --> doShift "1:web" -- Special-case the GIMP.
+	, resource 	=? "chrome" --> doShift "3:chrome" -- Special-case the GIMP.
+	, className =? "irsii" --> doShift "5:chat" -- Special-case the GIMP.
+	, className =? "Thunderbird" --> doShift "6:mail" -- Special-case the GIMP.
+	, manageDocks -- For xmobar and gnome-panel.
+	, manageHook gnomeConfig ] -- Gnome defaults.
 
 main = xmonad $ gnomeConfig
-	{
-		borderWidth					= 1,
-		manageHook  				= manageDocks
---		, layoutHook 				= spacing 0 $ avoidStruts $ Tall 1 (3/100) (1/2) 
+	{ borderWidth = 1
+	, workspaces  = myWorkspaces
+	, manageHook  = myHookManager
+	-- , layoutHook 				= spacing 0 $ avoidStruts $ Tall 1 (3/100) (1/2) 
 	} 
 	`additionalKeysP` 
 		[
